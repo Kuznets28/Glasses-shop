380d7ca2f2a2ae6468c7bfe77b3c07370071cf11
Gleb <star-1772@mail.ru>
2026-05-28 00:27:26 +0300

    cart and listener

--- src/main/java/org/example/servlet/SessionListener.java
+++ src/main/java/org/example/servlet/SessionListener.java
@@ -1,2 +1,21 @@
-package org.example.servlet;public class SessionListener {
+package org.example.servlet;
+
+import jakarta.servlet.annotation.WebListener;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+import org.example.Dto.UserSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@WebListener
+public class SessionListener implements HttpSessionListener {
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se){
+        UserSession user = (UserSession) se.getSession().getAttribute("UserSession");
+        if (user != null){
+
+        }
+    }
 }
--- src/main/java/org/example/servlet/cartServlet.java
+++ src/main/java/org/example/servlet/cartServlet.java
@@ -1,2 +1,23 @@
-package org.example.servlet;public class cart {
+package org.example.servlet;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/cart")
+public class cartServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/views/cart.jsp").forward(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
 }
