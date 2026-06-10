0d09e4c746e97ebae5c72bfdbb6a7a2b2109a46a
Gleb <star-1772@mail.ru>
2026-06-11 00:31:20 +0300

    cart and ...

--- src/main/createSql.sql
+++
@@ -1,6 +0,0 @@
-create table glasses (id serial primary key,
-					  sku varchar(50) unique,
-					  name varchar(50) unique,
-					  price int,
-					  pathToPhoto varchar(50));
-
---
+++ src/main/java/org/example/listener/ServletListener.java
@@ -0,0 +1,2 @@
+package org.example.Listener;public class ServletListener {
+}
